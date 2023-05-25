6e323db9d3fc03d2457e4b51feba56a5acc8a05f
Dardun <4dam.strobl@gmail.com>
2023-05-25 23:41:37 +0200

    pushing entire project for colalb

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $cz.cvut.fel.strobad1bielevan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
