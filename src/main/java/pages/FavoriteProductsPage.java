130bfb34561b9657557c27784d5df8fc4bd91883
Vanessa Bielena <bielevan@fel.cvut.cz>
2023-05-27 13:54:30 +0200

    New classes, first methods

---
+++ src/main/java/pages/FavoriteProductsPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class FavoriteProductsPage {
+
+    private WebDriver driver;
+
+    public FavoriteProductsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,15 +3,25 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 import java.sql.Driver;
 
 public class HomePage {
     private WebDriver driver;
-//
-//    @FindBy(css = "img[alt='Search Options']")
-//    private WebElement searchOptionsButton;
+
+    @FindBy(how = How.CSS, using = "body > div:nth-child(1) > div:nth-child(3) > header:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > div:nth-child(2)")
+    private WebElement loginButtonAnchor;
+
+    @FindBy(how = How.CSS, using = "body > div:nth-child(1) > div:nth-child(3) > header:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > div:nth-child(2)")
+    private WebElement cartButtonAnchor;
+
+    @FindBy(how = How.CSS, using = "body > div:nth-child(1) > div:nth-child(3) > header:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1) > div:nth-child(2)")
+    private WebElement favoritesButtonAnchor;
+
+    @FindBy(how = How.CSS, using = "body > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)")
+    private WebElement bandsButtonAnchor;
 //
 //    @FindBy(css = "div[class='cross-nav cross-nav--wide'] a[class='register-link flyout-caption']")
 //    private WebElement registerButton;
@@ -21,8 +31,34 @@ public class HomePage {
     public HomePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        driver.get("https://metalshop.cz");
+//        driver.get("https://metalshop.cz");
     }
+
+    public HomePage openShop() {
+        driver.get("https://metalshop.cz/");
+        return this;
+    }
+
+    public ShoppingCartPage clickShoppingCartOption() {
+        cartButtonAnchor.click();
+        return new ShoppingCartPage(driver);
+    }
+
+    public LoginPage clickLoginOption() {
+        loginButtonAnchor.click();
+        return new LoginPage(driver);
+    }
+
+    public FavoriteProductsPage clickFavoritesOption() {
+        favoritesButtonAnchor.click();
+        return new FavoriteProductsPage(driver);
+    }
+
+    public BandsPage clickBandsOption() {
+        bandsButtonAnchor.click();
+        return new BandsPage(driver);
+    }
+
 
 //    public pages.HomePage clickSearchOptions() throws InterruptedException {
 //        searchOptionsButton.click();
