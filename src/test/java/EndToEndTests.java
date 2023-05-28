47f3ee52664e25e7a0173d7342c63a2b6d6d7eec
Dardun <4dam.strobl@gmail.com>
2023-05-28 20:58:14 +0200

    added EndToEndTests.java

--- src/test/java/EndToEndTests.java
+++ src/test/java/EndToEndTests.java
@@ -3,12 +3,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.HomePage;
+
+import java.net.URISyntaxException;
 
 public class EndToEndTests {
 
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
     UtilTestClass utilTestClass = new UtilTestClass();
 
 
@@ -31,6 +37,21 @@ public class EndToEndTests {
      @AfterEach
     void teardown(){
          driver.quit();
-     }
+    }
 
+
+
+    @Test
+    public void firstTest() throws URISyntaxException, InterruptedException {
+        SimpleTests simpleTests = new SimpleTests();
+
+        HomePage homePage = new HomePage(driver);
+
+        utilTestClass.acceptCookies();
+        simpleTests.search("abc");
+
+        utilTestClass.clickLoginOption(driver);
+
+//        simpleTests.login
+    }
 }
